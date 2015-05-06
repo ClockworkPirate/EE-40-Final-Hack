4a306d337f14bf7e5bdb12045dc9400b5621b49a
ClockworkPirate <mguerrieri@outlook.com>
2015-05-06 09:32:03 -0700

    Finished

--- robot_server.py
+++ robot_server.py
@@ -1,33 +1,42 @@
-import os, cherrypy
-# from galileo import *
-
-# l = 4
-# r = 2
-#
-# pinMode(l, OUTPUT)
-# pinMode(r, OUTPUT)
+import os, random, string
+import cherrypy
+from galileo import *
 
 class RobotControl(object):
 	@cherrypy.expose
 	def index(self):
-		return open("index.html", "r").read()
+		return file("index.html")
 
-class RobotControlAPI(object):
+class RobotControlWebService(object):
 	exposed = True
 
 	@cherrypy.tools.accept(media='text/plain')
-	def POST(self, direction):
+	def GET(self):
+		return cherrypy.session["mystring"]
+	
+	def POST(self, direction=""):
 		print "POST"
-		# digitalWrite(l, HIGH)
-		# delay(1000)
-		# digitalWrite(l, LOW)
+		digitalWrite(l, HIGH)
+		delay(500)
+		digitalWrite(l, LOW)
+		return "POSTed"
 
 	def PUT(self, direction):
 		print "PUT"
 
+	def DELETE(self):
+		cherrypy.session.pop('mystring', None)
+
 if __name__ == '__main__':
+	l = 4
+	r = 2
+	
+	pinMode(l, OUTPUT)
+	pinMode(r, OUTPUT)
+	
 	conf = {
 		'/': {
+			'tools.sessions.on': True,
 			'tools.staticdir.root': os.path.abspath(os.getcwd())
 		},
 		'/control': {
@@ -41,5 +50,5 @@ if __name__ == '__main__':
 		}
 	}
 	page = RobotControl()
-	page.control = RobotControlAPI()
+	page.control = RobotControlWebService()
 	cherrypy.quickstart(page, '/', conf)
